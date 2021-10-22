5a4cf966e4a44a33d95733a50667f1fe7caeed80
dedsec995 <amitluhar49@gmail.com>
2021-10-22 14:53:08 +0530

    v1.2

--- M1/src/main/java/com/dedsec995/M1/RandomString.java
+++ M1/src/main/java/com/dedsec995/M1/RandomString.java
@@ -54,5 +54,42 @@ public static String getVinSpeed(int n)
 		
 }
 
+public static String getVin()
+{
 
+	// chose a Character and Integer random from this String
+	String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789";
+
+	// create StringBuffer size of AlphaNumericString
+	StringBuilder sb = new StringBuilder(17);
+
+	for (int i = 0; i < 17; i++) {
+		int index = (int)(AlphaNumericString.length() * Math.random());
+		sb.append(AlphaNumericString.charAt(index));
+	}
+	
+	// int strlen = sb.length();
+	// System.out.println(strlen);
+	
+	return sb.toString();
+	
+		
+}
+public static String getSpeed()
+{
+
+	String NumericString = "0123456789";
+	StringBuilder sb = new StringBuilder(3);
+
+	for (int i = 0; i < 3; i++) {
+		int index = (int)(NumericString.length() * Math.random());
+		sb.append(NumericString.charAt(index));
+	}	
+	// int strlen = sb.length();
+	// System.out.println(strlen);
+	
+	return sb.toString();
+	
+		
+}
 }
--- M1/src/main/java/com/dedsec995/M1/controller/KafkaController.java
+++ M1/src/main/java/com/dedsec995/M1/controller/KafkaController.java
@@ -1,11 +1,15 @@
 package com.dedsec995.M1.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
 import com.dedsec995.M1.RandomString;
 import com.dedsec995.M1.service.Producer;
 
@@ -21,19 +25,45 @@ public class KafkaController {
 
 
 	@PostMapping(value="/pro")
-    public void postBody(@RequestParam("vin") int vin,@RequestParam("freq") int freq) {
+    public void postBody(@RequestParam("vin") int vin,@RequestParam("freq") int freq,@RequestParam("same")Optional<String> same) {
         // System.out.println(vin);
         // System.out.println(freq);
         int secondsToSleep = freq;
 	    int i;
-		try{
-			for(i=0;i<vin;i++){
-			String result1 = RandomString.getVinSpeed(20);
-			producer.publishToTopic(result1);
-			Thread.sleep(secondsToSleep * 1000);
+		boolean isEqual = same.isPresent() && same.get().equals("y");
+		if(same.isEmpty()){
+			try{
+				for(i=0;i<vin;i++){
+				String result1 = RandomString.getVinSpeed(20);
+				producer.publishToTopic(result1);
+				Thread.sleep(secondsToSleep * 1000);
+				}
+			}catch(InterruptedException ie){
+				Thread.currentThread().interrupt();
 			}
-		}catch(InterruptedException ie){
-			Thread.currentThread().interrupt();
+		}
+		else if(isEqual){
+			String vinn = RandomString.getVin();
+			try{
+				for(i=0;i<vin;i++){
+				String speeed = RandomString.getSpeed();
+				producer.publishToTopic(vinn+speeed);
+				Thread.sleep(secondsToSleep * 1000);
+				}
+			}catch(InterruptedException ie){
+				Thread.currentThread().interrupt();
+			}
+		}
+		else{
+			try{
+				for(i=0;i<vin;i++){
+				String result1 = RandomString.getVinSpeed(20);
+				producer.publishToTopic(result1);
+				Thread.sleep(secondsToSleep * 1000);
+				}
+			}catch(InterruptedException ie){
+				Thread.currentThread().interrupt();
+			}
 		}
     }
 }
